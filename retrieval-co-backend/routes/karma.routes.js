3edf9796e978ec1bac0f8b00a8d6120c52fc1dca
abhi-vyakti <abhijeetindia06@gmail.com>
2026-07-06 00:55:13 +0530

    chore: remove redundant backend directory

--- retrieval-co-backend/routes/karma.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getLeaderboard } = require('../controllers/karma.controller');
-const protect = require('../middlewares/auth.middleware');
-
-// Public route or protected depending on preference. MVP uses public viewable board.
-router.get('/leaderboard', getLeaderboard);
-
-module.exports = router;
